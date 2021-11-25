608c4eca6a0f7e9ce0f3b2aceb82f18374a4789d
Nikolaj <53862286+Green-Sickness@users.noreply.github.com>
2021-11-25 19:00:54 +0100

    Update sketch_arduino.ino

--- sketch_arduino.ino
+++ sketch_arduino.ino
@@ -11,129 +11,188 @@ dht DHT; // opretter et objekt som udnytter dht library
 #define SOUND_BUZZER 10
 #define SOUND_READ A0
 
+// Denne define er til vores kanp på boardet
 #define BUTTON_BUZZ 8
 
 // opretter højeste grænser som vores målinger må have
-int templimt = 27;
-int humiditylimt = 50;
+int templimt = 30;
+int humiditylimt = 55;
+
+int adc;
+int dB;
 
 int period = 2000;
-unsigned long time_now = 0;
+unsigned long time_now = 250;
 
-int Buzzperiod = 2000;
-unsigned long time_nowBuzz = 0;
+bool allowBuzz = true;
 
-int buttonState = 0;  
+bool overValueTemp, overValueHum, overValueSound;
 
-void setup() {
-  //Definere at LED'erne skal sende data ikke modtage
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(LED_YELLOW, OUTPUT);
+bool alerted = false;
 
-  // Knappen og buzzeren
-  pinMode(BUTTON_BUZZ, INPUT);
-  pinMode(SOUND_BUZZER, OUTPUT);
+int buttonState = 0;
+int lastButtonState = 0;
 
-  // Definere at vores SOUND  
-  pinMode(SOUND_READ, INPUT);
+void setup()
+{
+    //Definere at LED'erne skal sende data ikke modtage
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_BLUE, OUTPUT);
+    pinMode(LED_YELLOW, OUTPUT);
 
+    // Knappen og buzzeren
+    pinMode(BUTTON_BUZZ, INPUT);
+    pinMode(SOUND_BUZZER, OUTPUT);
 
-  // Start serial monitor/plotter
-  Serial.begin(9600);
+    // Definere at vores SOUND
+    pinMode(SOUND_READ, INPUT);
+
+    // Start serial monitor/plotter
+    Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
     time_now = millis();
-   
-    // Kører voes temperature og luftfugtigheds sensor ude hvert 2000ms 
+
+    // Kører voes temperature og luftfugtigheds sensor ude hvert 2000ms
     checkTnH();
 
     // Kører alt kode i en zero delay loop
-    while(millis() < time_now + period){
-      checkSound();
-      buttonState = digitalRead(BUTTON_BUZZ);
+    while (millis() < time_now + period)
+    {
+        alert(true);
+        buttonState = digitalRead(BUTTON_BUZZ);
+
+        if (buttonState == 1 && lastButtonState == 0)
+        {
+            // Denne if statement / else, er ligesom en off and on switch fordi hvis allowBuzz er True bliver den false og hvis den er false bliver den True
+            if (allowBuzz)
+            {
+                allowBuzz = false;
+            }
+            else
+            {
+                allowBuzz = true;
+            }
+        }
+        lastButtonState = buttonState;
+        checkSound();
     }
-   ;  
-}
+};
 
-void checkTnH() {
+void checkTnH()
+{
     // chk er en variable som modtager data fra vores temperature/luftfugtighedssensor
-   int chk = DHT.read11(DHT11_PIN);
+    int chk = DHT.read11(DHT11_PIN);
 
-  // Hvis chk er værdien 0 så udskriver den de følgende data'er i "if statement'et"
-  if (chk == 0){
-    // prints our Temp every 2 secs
-    Serial.print("\nTemperature = ");
-    Serial.println(DHT.temperature);
-    checklimts(0);
+    // Hvis chk er værdien 0 så udskriver den de følgende data'er i "if statement'et"
+    if (chk == 0)
+    {
+        // prints our Temp every 2 secs
+        Serial.print("\nTemperature = ");
+        Serial.println(DHT.temperature);
+        checklimts(0);
 
-    Serial.print("\n");
-    // Prints our humidity every 2 secs
-    Serial.print("Humidity = ");
-    Serial.println(DHT.humidity);
-    checklimts(1);
+        Serial.print("\n");
+        // Prints our humidity every 2 secs
+        Serial.print("Humidity = ");
+        Serial.println(DHT.humidity);
+        checklimts(1);
 
-    Serial.print("\n");
-  }
+        Serial.print("\n");
+    }
 
-  // Printer en "error message" hvis vores modul ikke kunne læse noget 
-  else {
-    Serial.println("Error reading DHT11-module Failed!");
-    
-  }
- 
+    // Printer en "error message" hvis vores modul ikke kunne læse noget
+    else
+    {
+        Serial.println("Error reading DHT11-module Failed!");
+    }
 }
 
-void checklimts(int options){
+void checklimts(int options)
+{
+    // Cheker hvis vores temperature er for høj og i det tilfælde melder det i serial monitor'en
+    if (DHT.temperature > templimt && options == 0)
+    {
+        Serial.print("Temperature is too HIGH!!\n");
 
-  // Cheker hvis vores temperature er for høj og i det tilfælde melder det i serial monitor'en 
-  if (DHT.temperature > templimt && options == 0){
-     Serial.print("Temperature is too HIGH!!\n");
+        // tænder vores Røde led pære hvis temperaturen overskrider grænsen
+        digitalWrite(LED_RED, HIGH);
+        // Sætter vores bool til en false for at fortælle at den er over den tilldte værdi
+        overValueTemp = true;
+    }
+    else if (options == 0)
+    {
+        // Slukker LED'en når temperaturen falder under grænsen igen
+        digitalWrite(LED_RED, LOW);
 
-     // tænder vores Røde led pære hvis temperaturen overskrider grænsen 
-     digitalWrite(LED_RED, HIGH);
-  }
-  else if (options == 0){
-    // Slukker LED'en når temperaturen falder under grænsen igen
-    digitalWrite(LED_RED, LOW);
-  }
+        // Sætter vores bool til en false for at fortælle at den ikke er over den tilldte værdi
+        overValueTemp = false;
+    }
 
-  
-  // Cheker hvis vores luftfugtigheden er for høj og i det tilfælde melder det i serial monitor'en
-  if (DHT.humidity > humiditylimt && options == 1){
-     Serial.print("Humidity is too HIGH!!");
+    // Cheker hvis vores luftfugtigheden er for høj og i det tilfælde melder det i serial monitor'en
+    if (DHT.humidity > humiditylimt && options == 1)
+    {
+        Serial.print("Humidity is too HIGH!!");
 
-     // Tænder vores blå led pære hvis luftfugtigheden overskrider grænsen 
-     digitalWrite(LED_BLUE, HIGH);
-  }
-  else if (options == 1){
-      // Slukker LED'en når Luftfugtigheden falder under grænsen igen
-     digitalWrite(LED_BLUE, LOW);
-  }
+        // Tænder vores blå led pære hvis luftfugtigheden overskrider grænsen
+        digitalWrite(LED_BLUE, HIGH);
+        // Sætter vores bool til en true for at fortælle at den er over den tilldte værdi
+        overValueHum = true;
+    }
+    else if (options == 1)
+    {
+        // Slukker LED'en når Luftfugtigheden falder under grænsen igen
+        digitalWrite(LED_BLUE, LOW);
+
+        // Sætter vores bool til en false for at fortælle at den ikke er over den tilldte værdi
+        overValueHum = false;
+    }
 }
 
-int adc;
-int dB, PdB; //the variable that will hold the value read from the microphone each time
+bool flag = false;
+int cont = 0;
+void alert(bool shouldbuzz)
+{
+    // Her checker vi hvis den må og skulle buzz og derefter hvis nogle af tingene er over den tilldt værdi, Hvis værdien af nogle af dem sender vi et Buzz.
+    if (allowBuzz && shouldbuzz && (overValueTemp || overValueHum || overValueSound))
+    {
+        tone(SOUND_BUZZER, 500, 500);
 
-void checkSound() {
-  float Analog, lastAnalog;
+        tone(SOUND_BUZZER, 0, 3000);
 
-  float lastdiff;
+        tone(SOUND_BUZZER, 500, 500);
+    }
+}
 
-  adc = analogRead(SOUND_READ); //Read the ADC value from amplifer
-  dB = (adc+83.2073) / 11.003; //Convert ADC value to dB using Regression values
-  
-  Analog = analogRead (SOUND_READ);
+// your loop-limited stuff here
 
-  double diff = abs(Analog - lastAnalog);
-  
-  if (diff >= 550) {
-  Serial.print("dB-Level=");
-  Serial.println(dB, DEC);
-  digitalWrite(LED_YELLOW, HIGH);
-  } else{
-      digitalWrite(LED_YELLOW, LOW);
-  }
-  lastAnalog = Analog;
+void checkSound()
+{
+    float Analog, lastAnalog;
+
+    float lastdiff;
+
+    adc = analogRead(SOUND_READ); //Read the ADC value from amplifer
+    dB = (adc + 83.2073) / 11.003; //Convert ADC value to dB using Regression values
+
+    Analog = analogRead(SOUND_READ);
+
+    double diff = abs(Analog - lastAnalog);
+
+    if (diff >= 605)
+    {
+        Serial.print("dB-Level=");
+        Serial.println(dB, DEC);
+        alert(true);
+        overValueSound = true;
+        digitalWrite(LED_YELLOW, HIGH);
+    }
+    else
+    {
+        overValueSound = false;
+        digitalWrite(LED_YELLOW, LOW);
+    }
+    lastAnalog = Analog;
 }
